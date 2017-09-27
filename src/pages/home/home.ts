ed2a3b0439851ddd98f07c9b2b8622d4a672429c
jadebard <jadebard@indiana.edu>
2017-09-27 16:51:35 -0400

    First Cards
    First few cards. Need to add more

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,43 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+
+    cards = [
+        {
+            imgPath: "JwTKjXN.jpg",
+            title: "Pink floyd",
+            desc: "This is a cool background created by user: XYZ1000c",
+            favorited: false
+        },
+        {
+            imgPath: "images.jpg",
+            title: "Bridge",
+            desc: "This is a cool background created by user: ABCDD1836!",
+            favorited: false
+        },
+        {
+            imgPath: "download.jpg",
+            title: "Awesome",
+            desc: "This is a cool background created by user: jadebard",
+            favorited: false
+        }
+    ];
+
   constructor(public navCtrl: NavController) {
 
   }
+
+    clickFavorite(card) {
+        let index = this.cards.indexOf(card);
+
+        if(index > -1) {
+            if (card.favorited) {
+                this.cards[index].favorited = false;
+            } else {
+                this.cards[index].favorited = true;
+            }
+        }
+    }
+
 
 }
