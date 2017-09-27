4cc9e43e26d9262fae34f7f0fbcf87de70fcf191
jadebard <jadebard@indiana.edu>
2017-09-27 17:49:07 -0400

    FINISHED
    Finished the assignment

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,20 +13,72 @@ export class HomePage {
             imgPath: "JwTKjXN.jpg",
             title: "Pink floyd",
             desc: "This is a cool background created by user: XYZ1000c",
-            favorited: false
+            favorited: false,
+            shared: false
         },
         {
             imgPath: "images.jpg",
             title: "Bridge",
             desc: "This is a cool background created by user: ABCDD1836!",
-            favorited: false
+            favorited: false,
+            shared: false
         },
         {
             imgPath: "download.jpg",
             title: "Awesome",
             desc: "This is a cool background created by user: jadebard",
-            favorited: false
-        }
+            favorited: false,
+            shared: false
+        },
+        {
+            imgPath: "canal.jpg",
+            title: "Bridge",
+            desc: "This is a cool background created by user: ABCDD1836!",
+            favorited: false,
+            shared: false
+        },
+        {
+            imgPath: "moonbeer.jpg",
+            title: "Bridge",
+            desc: "This is a cool background created by user: ABCDD1836!",
+            favorited: false,
+            shared: false
+        },
+        {
+            imgPath: "mountain.jpg",
+            title: "Bridge",
+            desc: "This is a cool background created by user: ABCDD1836!",
+            favorited: false,
+            shared: false
+        },
+        {
+            imgPath: "nightmountain.jpg",
+            title: "Bridge",
+            desc: "This is a cool background created by user: ABCDD1836!",
+            favorited: false,
+            shared: false
+        },
+        {
+            imgPath: "skyline.jpg",
+            title: "Bridge",
+            desc: "This is a cool background created by user: ABCDD1836!",
+            favorited: false,
+            shared: false
+        },
+        {
+            imgPath: "sloth.jpg",
+            title: "Bridge",
+            desc: "This is a cool background created by user: ABCDD1836!",
+            favorited: false,
+            shared: false
+        },
+        {
+            imgPath: "windows.jpg",
+            title: "Bridge",
+            desc: "This is a cool background created by user: ABCDD1836!",
+            favorited: false,
+            shared: false
+        },
     ];
 
   constructor(public navCtrl: NavController) {
@@ -43,6 +95,23 @@ export class HomePage {
                 this.cards[index].favorited = true;
             }
         }
+    }
+
+    clickShared(card) {
+        let index = this.cards.indexOf(card);
+
+        if(index > -1) {
+            if (card.shared) {
+                this.cards[index].shared = false;
+            } else {
+                this.cards[index].shared = true;
+            }
+        }
+    }
+
+    deleteCard(card) {
+        let index = this.cards.indexOf(card);
+        this.cards.splice(index, 1)
     }
 
 
